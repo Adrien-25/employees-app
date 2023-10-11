6fb57a00f8018660113f8de6a7c78cd22c7eb022
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2023-10-11 22:04:55 +0200

    Employee management - Laravel

---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::middleware('splade')->group(function () {
+    // Registers routes to support the interactive components...
+    Route::spladeWithVueBridge();
+
+    // Registers routes to support password confirmation in Form and Link components...
+    Route::spladePasswordConfirmation();
+
+    // Registers routes to support Table Bulk Actions and Exports...
+    Route::spladeTable();
+
+    // Registers routes to support async File Uploads with Filepond...
+    Route::spladeUploads();
+
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->middleware(['verified'])->name('dashboard');
+
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+    Route::middleware('auth')->prefix('/admin')->name('admin.')->group(function () {
+        Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+        Route::resource('/users',UserController::class);
+        Route::resource('/employees',UserController::class);
+        Route::resource('/countries',UserController::class);
+        Route::resource('/states',UserController::class);
+        Route::resource('/cities',UserController::class);
+        Route::resource('/departments',UserController::class);
+    });
+
+
+    require __DIR__.'/auth.php';
+});
