841ee0a4887ff58f57d4fd2e60d230ea1d61cb04
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2023-10-12 23:43:46 +0200

    Delete user Modal

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -9,7 +9,9 @@
         @cell('action',$user)
         <div class="space-x-2">
             <Link href="{{ route('admin.users.edit',$user)}}" class="px-3 py-2 text-green-400 hover:text-green-700 font-semibold rounded-md">Edit</Link>
-            <Link href="{{ route('admin.users.destroy',$user)}}" method="DELETE" class="px-3 py-2 text-green-400 hover:text-green-700 font-semibold rounded-md">Delete</Link>
+            <Link href="{{ route('admin.users.destroy', $user) }}" method="DELETE" confirm="Delete the user" confirm-text="Are you sure?" confirm-button="Yes" cancel-button="No" class="text-red-400 hover:text-red-700 font-semibold">
+            Delete
+            </Link>
         </div>
         @endcell
     </x-splade-table>
