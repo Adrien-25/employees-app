e032b8f67bd661973d9e712f680cd494327e4fee
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2023-10-18 00:29:33 +0200

    Create Admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,13 +15,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(1)->create();
-        $role = Role::create(['name' => 'admin']);
+        // User::factory(1)->create();
+        // $role = Role::create(['name' => 'admin']);
 
-        $user = User::factory()->create([
-            'first_name' => 'Admin',
-            'email' => 'admin@example.com',
-        ]);
-        $user->assignRole($role);
+        // $user = User::factory()->create([
+        //     'first_name' => 'Admin',
+        //     'email' => 'admin@example.com',
+        // ]);
+        // $user->assignRole($role);
+
+
+        // ============================
+
+
+        // $user = User::factory()->create([
+        //     'first_name' => 'Admin',
+        //     'email' => 'admin@example.com',
+        //     'password' => 'adminadmin',
+        // ]);
+        
+        // $role = Role::findByName('admin', 'web');
+        
+        // if ($role) {
+        //     $user->assignRole($role);
+        // } else {
+
+        // }
     }
 }
